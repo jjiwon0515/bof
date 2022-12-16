f904e375663283963a831b92253bd4cbf6c91608
jiwon <107084512+huboca@users.noreply.github.com>
2022-12-16 20:30:43 +0900

    Update bof.c

--- bof.c
+++ bof.c
@@ -3,3 +3,17 @@
 #include<stdlib.h>
 
 void func(int key){
+  char overflow[32];
+  printf("overflow: ");
+  gets(overflow);
+  if(key==0xcafebabe){
+    system("/bin/sh");
+  }
+  else{
+    printf("Nah...\n);
+  }
+}
+int main(int argc, char * argv[]){
+  func(0xdeadbeef);
+}
+         
