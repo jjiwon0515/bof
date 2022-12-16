e23560abb42ea5595ccc202a222f09ab7f770fe8
jiwon <107084512+huboca@users.noreply.github.com>
2022-12-16 07:20:50 -0500

    Update system.c

--- function/system.c
+++ function/system.c
@@ -1,32 +1,14 @@
 #system(): 명령어를 수행하는 함수. 프로그램을 실행하는 함수. 
 #예제를 통해서 봐야겠다
 
-#target.exe
 #include<stdio.h>
-
-int main(void)
-{
-  printf("타겟\n");
-  return 3;
-}
-
-#사용 예시
 #include<stdlib.h>
-#include<stdio.h>
 
-int main(void)
-{
-  int re;
-  re = system("notepad");
-  printf("notepad return : %d\n",re);
-  re = system("target");
-  printf("target return : %d\n",re);
-
+int main(){
+  int ret = system("ls -l");
+  printf("ret : %d\n",ret);
   return 0;
 }
 
-#결과
 
-#notpad return : 0 실제로 notepad라는 경로는 없었기 때문에 실패 -> 0을 반환
-#타겟
-#target return : 3 
+#결과
